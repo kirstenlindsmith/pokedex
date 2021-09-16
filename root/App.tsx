8203db323e54f555895fb681e93589b6803d4f88
kirstenlindsmith <kirstenlindsmith@gmail.com>
2021-09-16 17:24:35 -0400

    wip

--- root/App.tsx
+++ root/App.tsx
@@ -1,21 +1,25 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Home from './components/Home';
-import PokemonDetail from './components/PokemonDetail';
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
-    <Switch>
-      <Route path='/pokemon/:id' exact>
-        <PokemonDetail />
-      </Route>
-      <Route path='/'>
-        <Home />
-      </Route>
-    </Switch>
-  </BrowserRouter>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
